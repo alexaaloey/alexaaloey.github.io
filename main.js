05407f8aa8c1050f53944870f55c11206c3d2633
Alexa <L21270629@tuxtla.tecnm.mx>
2024-08-15 21:56:17 -0600

    versión 2

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString('<h2>Desarrolladora Front-End (Aprendiz)</h2>')
+typewriter.typeString('<h2>Desarrolladora Front-End</h2>')
     .pauseFor(2500)
     .deleteAll()
    
